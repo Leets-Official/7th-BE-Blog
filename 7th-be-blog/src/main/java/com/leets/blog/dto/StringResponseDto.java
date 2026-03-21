28408146ed4b04501fae1d17dda5125a55dc8e8e
김동빈 <kdobi87@naver.com>
2026-03-21 20:19:53 +0900

    [Feat] 1주차 DTO 추가 및  healthCheck API 구현

---
+++ 7th-be-blog/src/main/java/com/leets/blog/dto/StringResponseDto.java
@@ -0,0 +1,10 @@
+package com.leets.blog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class StringResponseDto {
+    private String result;
+}
