49b9f5b086c4e63bd80bec860a7a50badff9116a
변승현 <qustmdgus1212@gmail.com>
2026-03-22 15:35:00 +0900

    return 메시지 변경

--- src/main/java/com/example/demo/controller/ControllerHealth.java
+++ src/main/java/com/example/demo/controller/ControllerHealth.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ControllerHealth {
     @GetMapping("/health")
     public String health() {
-        return "Server is running";
+        return "OK";
     }
 }
 
