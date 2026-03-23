d76f589e7abf0377accbc797f336fd7595f11459
lsh <lsh0131pianist@gmail.com>
2026-03-23 18:17:24 +0900

    [feat] 헬스 체크 api 구현

---
+++ src/main/java/com/example/blog7th/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.example.blog7th.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "ok";
+    }
+}
