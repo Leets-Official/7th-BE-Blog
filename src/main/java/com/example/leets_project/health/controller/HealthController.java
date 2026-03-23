d938049c143a80821123cdd397647d818ec8bbe3
이교형 <kevin1371@naver.com>
2026-03-23 20:21:28 +0900

    [Feat] 1주차 healthCheck API 구현

--- src/main/java/com/example/leets_project/health/controller/HealthController.java
+++ src/main/java/com/example/leets_project/health/controller/HealthController.java
@@ -1,4 +1,19 @@
 package com.example.leets_project.health.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Health Check API", description = "서버 정상 작동 확인용 API")
+@RestController
+@RequestMapping("/api")
 public class HealthController {
+
+    @Operation(summary = "서버 정상 작동 확인", description = "서버가 정상 작동하는지 확인합니다.")
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
 }
