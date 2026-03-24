906b3aac94c464878661d7cb7f37ab2697a90225
lsh <lsh0131pianist@gmail.com>
2026-03-24 13:27:20 +0900

    [refactor] URL 체계화

--- src/main/java/com/example/blog7th/controller/HealthController.java
+++ src/main/java/com/example/blog7th/controller/HealthController.java
@@ -2,8 +2,10 @@ package com.example.blog7th.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/api/health")
 public class HealthController {
 
     @GetMapping("/health")
