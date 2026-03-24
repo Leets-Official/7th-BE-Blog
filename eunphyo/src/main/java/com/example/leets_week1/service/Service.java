ff29809aa2467ea584ae0837f43e72e481811695
vyfhfhd <lucy.gachon.ac.kr>
2026-03-24 13:18:57 +0900

    1week assignment committt2

--- eunphyo/src/main/java/com/example/leets_week1/service/Service.java
+++ eunphyo/src/main/java/com/example/leets_week1/service/Service.java
@@ -2,7 +2,7 @@ package com.example.leets_week1.service;
 
 import com.example.leets_week1.dto.Response_dto;
 
-@org.springframework.stereotype.Service
+@Service
 public class Service {
     public Response_dto String(String input){
 
