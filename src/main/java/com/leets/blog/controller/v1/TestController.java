a0d589db8634adc6d5868df25f0aff4c5edbfc75
HyeonSeong <gus8131@naver.com>
2026-03-25 23:04:01 +0900

    [fix] @RequestBody 설정하여 JSON 형태 수집

--- src/main/java/com/leets/blog/controller/v1/TestController.java
+++ src/main/java/com/leets/blog/controller/v1/TestController.java
@@ -4,6 +4,7 @@ import com.leets.blog.controller.dto.StringRequest;
 import com.leets.blog.controller.dto.StringResponse;
 import com.leets.blog.service.DtoConverter;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,7 +17,7 @@ public class TestController {
     }
 
     @PostMapping("/string/repeat")
-    public StringResponse stringRepeat(StringRequest stringRequest) {
+    public StringResponse stringRepeat(@RequestBody StringRequest stringRequest) {
 
         StringResponse result = dtoConverter.convert(stringRequest);
 
