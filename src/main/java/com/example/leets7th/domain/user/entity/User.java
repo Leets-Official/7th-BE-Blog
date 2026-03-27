3940e3c542dff226fab74813b2123c3d359f2b92
kangyeson <yesonkang@gmail.com>
2026-03-28 03:18:50 +0900

    [Feat] Comment, Post, User Entity 추가

---
+++ src/main/java/com/example/leets7th/domain/user/entity/User.java
@@ -0,0 +1,29 @@
+package com.example.leets7th.domain.user.entity;
+
+import com.example.leets7th.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "password", nullable = false, length = 255)
+    private String password;
+
+    @Column(name = "email", nullable = false, length = 50)
+    private String email;
+}
