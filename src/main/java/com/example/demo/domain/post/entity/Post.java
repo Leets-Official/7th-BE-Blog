77b662e581c09ca89a695925c151f6f4ae71d9f3
변승현 <qustmdgus1212@gmail.com>
2026-03-30 20:11:28 +0900

    Post 엔티티 생성 및 User 연관관계 설정

---
+++ src/main/java/com/example/demo/domain/post/entity/Post.java
@@ -0,0 +1,38 @@
+package com.example.demo.domain.post.entity;
+
+import com.example.demo.domain.comment.entity.Comment;
+import com.example.demo.domain.user.entity.User;
+import com.example.demo.global.entity.BaseEntity;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Post extends BaseEntity {
+
+    // 포스트 아이디
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 유저 아이디
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    // 제목
+    @Column(nullable = false, length = 30)
+    private String title;
+
+    // 내용
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    // 사진
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
+}
