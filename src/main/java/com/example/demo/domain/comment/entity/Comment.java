1cc4b4e7a8c23b3adebbf5278e1b78d87dbea92a
변승현 <qustmdgus1212@gmail.com>
2026-03-30 20:11:44 +0900

    Comment 엔티티 생성 및 Post/User 연관관계 설정

---
+++ src/main/java/com/example/demo/domain/comment/entity/Comment.java
@@ -0,0 +1,31 @@
+package com.example.demo.domain.comment.entity;
+
+import com.example.demo.domain.post.entity.Post;
+import com.example.demo.domain.user.entity.User;
+import com.example.demo.global.entity.BaseEntity;
+import jakarta.persistence.*;
+
+@Entity
+public class Comment extends BaseEntity {
+
+    // 댓글 아이디
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 유저 아이디
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    // 포스트 아이디
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    // 내용
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+
+}
