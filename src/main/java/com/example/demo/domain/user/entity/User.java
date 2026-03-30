e0af8c596324e223f3ccea720e9f7397b1aba7ab
변승현 <qustmdgus1212@gmail.com>
2026-03-30 20:09:47 +0900

    User 엔티티 생성 및 기본 필드/연관관계 설정

---
+++ src/main/java/com/example/demo/domain/user/entity/User.java
@@ -0,0 +1,37 @@
+package com.example.demo.domain.user.entity;
+
+import com.example.demo.domain.comment.entity.Comment;
+import com.example.demo.domain.post.entity.Post;
+import com.example.demo.global.entity.BaseEntity;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class User extends BaseEntity {
+
+    // 유저 아이디
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 이름
+    @Column(nullable = false, length = 10)
+    private String name;
+
+    // 성별
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
+
+    // 나이
+    @Column
+    private Integer age;
+
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user")
+    private List<Comment> comments = new ArrayList<>();
+}
