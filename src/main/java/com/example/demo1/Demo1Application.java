e4413883e3278b9bac39863f81bcd20a17da3e63
ruchan04 <ruchan0@gachon.ac.kr>
2026-03-31 15:31:17 +0900

    feat: JPA Auditing 활성화 및 DB 설정 복구

--- src/main/java/com/example/demo1/Demo1Application.java
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//@EnableJpaAuditing // 생성일/수정일 자동 생성을 위해 꼭 필요해요!
+@EnableJpaAuditing // 생성일/수정일 자동 생성을 위해 꼭 필요해요!
 @SpringBootApplication // (exclude = ...) 부분을 지워서 DB 자동 설정을 활성화합니다.
 public class Demo1Application {
     public static void main(String[] args) {
