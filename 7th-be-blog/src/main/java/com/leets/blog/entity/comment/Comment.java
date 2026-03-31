7403e4b596cdc888efd4a82da3568c86f4c2b713
김동빈 <kdobi87@naver.com>
2026-03-31 20:20:13 +0900

    [FEAT] Entity 생성

--- 7th-be-blog/src/main/java/com/leets/blog/entity/comment/Comment.java
+++ 7th-be-blog/src/main/java/com/leets/blog/entity/comment/Comment.java
@@ -1,4 +1,35 @@
 package com.leets.blog.entity.comment;
 
-public class Comment {
+import com.leets.blog.entity.BaseEntity;
+import com.leets.blog.entity.post.Post;
+import com.leets.blog.entity.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "comments")
+public class Comment extends BaseEntity {
+
+    // - PK
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // - 내용
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    // - FK - user
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    // - FK - post
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
 }
--- 7th-be-blog/src/main/java/com/leets/blog/entity/post/Post.java
+++ 7th-be-blog/src/main/java/com/leets/blog/entity/post/Post.java
@@ -1,4 +1,41 @@
 package com.leets.blog.entity.post;
 
-public class Post {
+import com.leets.blog.entity.BaseEntity;
+import com.leets.blog.entity.comment.Comment;
+import com.leets.blog.entity.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "posts")
+public class Post extends BaseEntity {
+
+    // - pk
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // - 제목
+    @Column(nullable = false)
+    private String title;
+
+    // - 내용
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    // - FK - 유저
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    // Post - Comment (1:N)
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
 }
