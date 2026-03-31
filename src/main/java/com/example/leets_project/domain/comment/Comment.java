370ceb43c0345f8547fcbe5e07b038423e3f9de4
이교형 <kevin1371@naver.com>
2026-03-31 23:02:23 +0900

    [feat] 2주차 User, Post, Comment 엔티티 구현

---
+++ src/main/java/com/example/leets_project/domain/comment/Comment.java
@@ -0,0 +1,50 @@
+package com.example.leets_project.domain.comment;
+
+import com.example.leets_project.domain.post.Post;
+import com.example.leets_project.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @Column(nullable = false, length = 255)
+    private String content;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public Comment(User user, Post post, String content) {
+        this.user = user;
+        this.post = post;
+        this.content = content;
+    }
+}
