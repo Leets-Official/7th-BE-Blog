a1f6872a3c4c38f2fc65f464b86ae932e535bb8c
kimjihoon <jihungim500@gmail.com>
2026-04-07 01:02:37 +0900

    [Chore] #93 댓글 컨트롤러 세팅

---
+++ src/main/java/com/example/leets_7th/domain/comment/controller/docs/CommentControllerDocs.java
@@ -0,0 +1,7 @@
+package com.example.leets_7th.domain.comment.controller.docs;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Comment", description = "댓글 API")
+public interface CommentControllerDocs {
+}
