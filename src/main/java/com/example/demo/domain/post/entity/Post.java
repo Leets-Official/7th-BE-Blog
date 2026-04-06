b976a1cc2bd85c6d8b7d1a0dd174c0f7cdc4b915
변승현 <qustmdgus1212@gmail.com>
2026-04-06 21:22:33 +0900

    게시글 엔티티 및 레포지토리 추가

--- src/main/java/com/example/demo/domain/post/entity/Post.java
+++ src/main/java/com/example/demo/domain/post/entity/Post.java
@@ -16,28 +16,44 @@ import java.util.List;
 @Entity
 public class Post extends BaseEntity {
 
-    // 포스트 아이디
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // 유저 아이디
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    // 제목
     @Column(nullable = false, length = 30)
     private String title;
 
-    // 내용
     @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
 
-    // 사진
     @Column(name = "image_url")
     private String imageUrl;
 
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
+
+    private Post(User user, String title, String content, String imageUrl) {
+        this.user = user;
+        this.title = title;
+        this.content = content;
+        this.imageUrl = imageUrl;
+    }
+
+    public static Post of(User user, String title, String content, String imageUrl) {
+        return new Post(user, title, content, imageUrl);
+    }
+
+    public void update(String title, String content, String imageUrl) {
+        if (title != null && !title.isBlank()) {
+            this.title = title;
+        }
+        if (content != null && !content.isBlank()) {
+            this.content = content;
+        }
+        this.imageUrl = imageUrl;
+    }
 }
--- src/main/java/com/example/demo/domain/user/entity/User.java
+++ src/main/java/com/example/demo/domain/user/entity/User.java
@@ -2,7 +2,6 @@ package com.example.demo.domain.user.entity;
 
 import com.example.demo.domain.comment.entity.Comment;
 import com.example.demo.domain.post.entity.Post;
-import com.example.demo.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -14,25 +13,15 @@ import java.util.List;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class User extends BaseEntity {
+@Table(name = "user")
+public class User {
 
-    // 유저 아이디
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // 이름
-    @Column(nullable = false, length = 10)
-    private String name;
-
-    // 성별
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private Gender gender;
-
-    // 나이
-    @Column
-    private Integer age;
+    @Column(name = "nickname", nullable = false, length = 30)
+    private String nickname;
 
     @OneToMany(mappedBy = "user")
     private List<Post> posts = new ArrayList<>();
