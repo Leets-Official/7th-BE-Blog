74ef73454cb0710388cac93b8b18086390c263e5
kimjihoon <jihungim500@gmail.com>
2026-04-07 01:03:57 +0900

    [Feat] #93 게시글 컨트롤러 인터페이스 및 구현체 생성

---
+++ src/main/java/com/example/leets_7th/domain/post/controller/PostController.java
@@ -0,0 +1,80 @@
+package com.example.leets_7th.domain.post.controller;
+
+import com.example.leets_7th.common.response.ApiResponse;
+import com.example.leets_7th.common.status.SuccessStatus;
+import com.example.leets_7th.domain.post.controller.docs.PostControllerDocs;
+import com.example.leets_7th.domain.post.dto.request.CreatePostRequest;
+import com.example.leets_7th.domain.post.dto.request.UpdatePostRequest;
+import com.example.leets_7th.domain.post.dto.response.CreatePostResponse;
+import com.example.leets_7th.domain.post.dto.response.GetPostDetailResponse;
+import com.example.leets_7th.domain.post.dto.response.GetPostResponse;
+import com.example.leets_7th.domain.post.dto.response.UpdatePostResponse;
+import com.example.leets_7th.domain.post.service.PostCommandService;
+import com.example.leets_7th.domain.post.service.PostQueryService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/api/posts")
+@RestController
+@Validated
+@RequiredArgsConstructor
+public class PostController implements PostControllerDocs {
+
+    private final PostCommandService postCommandService;
+    private final PostQueryService postQueryService;
+
+    @Override
+    @GetMapping
+    public ResponseEntity<ApiResponse<GetPostResponse>> getAllPost(
+            @RequestParam Long userId,
+            @RequestParam int page,
+            @RequestParam int size
+    ) {
+        GetPostResponse response = postQueryService.getAllPost(page, size);
+        return ApiResponse.success(SuccessStatus.GET_ALL_POST_SUCCESS, response);
+    }
+
+    @Override
+    @GetMapping("/{postId}")
+    public ResponseEntity<ApiResponse<GetPostDetailResponse>> getDetailPost(
+            @RequestParam Long userId,
+            @Valid @PathVariable Long postId
+    ) {
+        GetPostDetailResponse response = postQueryService.getPostDetail(postId);
+        return ApiResponse.success(SuccessStatus.GET_POST_DETAIL_SUCCESS, response);
+    }
+
+    @Override
+    @PostMapping
+    public ResponseEntity<ApiResponse<CreatePostResponse>> createPost(
+            @RequestParam Long userId,
+            @RequestBody @Valid CreatePostRequest request
+    ) {
+        CreatePostResponse response = postCommandService.createPost(userId, request);
+        return ApiResponse.success(SuccessStatus.CREATE_POST_SUCCESS, response);
+    }
+
+    @Override
+    @PatchMapping("/{postId}")
+    public ResponseEntity<ApiResponse<UpdatePostResponse>> updatePost(
+            @RequestParam Long userId,
+            @PathVariable Long postId,
+            @Valid @RequestBody UpdatePostRequest request
+    ) {
+        UpdatePostResponse response = postCommandService.updatePost(userId, postId, request);
+        return ApiResponse.success(SuccessStatus.UPDATE_POST_SUCCESS, response);
+    }
+
+    @Override
+    @DeleteMapping("/{postId}")
+    public ResponseEntity<ApiResponse<Void>> deletePost(
+            @RequestParam Long userId,
+            @Valid @PathVariable Long postId
+    ) {
+        postCommandService.deletePost(userId, postId);
+        return ApiResponse.success(SuccessStatus.DELETE_POST_SUCCESS);
+    }
+}
