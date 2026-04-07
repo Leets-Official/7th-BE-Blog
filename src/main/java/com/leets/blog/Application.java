5709f45490c077cbbb8fcc45c9ad4403256c7908
yukyoungs <syk9848@gmail.com>
2026-04-07 18:16:41 +0900

    refactor: JPA Auditing 설정 분리

--- src/main/java/com/leets/blog/Application.java
+++ src/main/java/com/leets/blog/Application.java
@@ -3,12 +3,9 @@ package com.leets.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing      // JPA Auditing
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
