2eb3fd65fb697587cae7d868a9b55e676253a4af
jihoonkim501 <jihungim500@gmail.com>
2026-04-07 15:41:50 +0900

    [Refactor] #93 PostController 파라미터 -> DTO 로 변환

--- src/main/java/com/example/leets_7th/domain/post/controller/PostController.java
+++ src/main/java/com/example/leets_7th/domain/post/controller/PostController.java
@@ -4,6 +4,7 @@ import com.example.leets_7th.common.response.ApiResponse;
 import com.example.leets_7th.common.status.SuccessStatus;
 import com.example.leets_7th.domain.post.controller.docs.PostControllerDocs;
 import com.example.leets_7th.domain.post.dto.request.CreatePostRequest;
+import com.example.leets_7th.domain.post.dto.request.GetPostRequest;
 import com.example.leets_7th.domain.post.dto.request.UpdatePostRequest;
 import com.example.leets_7th.domain.post.dto.response.CreatePostResponse;
 import com.example.leets_7th.domain.post.dto.response.GetPostDetailResponse;
@@ -29,11 +30,9 @@ public class PostController implements PostControllerDocs {
     @Override
     @GetMapping
     public ResponseEntity<ApiResponse<GetPostResponse>> getAllPost(
-            @RequestParam Long userId,
-            @RequestParam int page,
-            @RequestParam int size
+            @Valid @ModelAttribute GetPostRequest request
     ) {
-        GetPostResponse response = postQueryService.getAllPost(page, size);
+        GetPostResponse response = postQueryService.getAllPost(request);
         return ApiResponse.success(SuccessStatus.GET_ALL_POST_SUCCESS, response);
     }
 
@@ -41,9 +40,9 @@ public class PostController implements PostControllerDocs {
     @GetMapping("/{postId}")
     public ResponseEntity<ApiResponse<GetPostDetailResponse>> getDetailPost(
             @RequestParam Long userId,
-            @Valid @PathVariable Long postId
+            @PathVariable Long postId
     ) {
-        GetPostDetailResponse response = postQueryService.getPostDetail(postId);
+        GetPostDetailResponse response = postQueryService.getPostDetail(userId, postId);
         return ApiResponse.success(SuccessStatus.GET_POST_DETAIL_SUCCESS, response);
     }
 
--- src/main/java/com/example/leets_7th/domain/post/controller/docs/PostControllerDocs.java
+++ src/main/java/com/example/leets_7th/domain/post/controller/docs/PostControllerDocs.java
@@ -2,6 +2,7 @@ package com.example.leets_7th.domain.post.controller.docs;
 
 import com.example.leets_7th.common.response.ApiResponse;
 import com.example.leets_7th.domain.post.dto.request.CreatePostRequest;
+import com.example.leets_7th.domain.post.dto.request.GetPostRequest;
 import com.example.leets_7th.domain.post.dto.request.UpdatePostRequest;
 import com.example.leets_7th.domain.post.dto.response.CreatePostResponse;
 import com.example.leets_7th.domain.post.dto.response.GetPostDetailResponse;
@@ -19,16 +20,14 @@ public interface PostControllerDocs {
     @Operation(summary = "게시글 목록 조회", description = "게시글 리스트를 조회합니다.")
     @GetMapping
     ResponseEntity<ApiResponse<GetPostResponse>> getAllPost(
-            @RequestParam Long userId,
-            @RequestParam int page,
-            @RequestParam int size
+            @Valid @ModelAttribute GetPostRequest request
     );
 
     @Operation(summary = "게시글 상세 조회", description = "특정 게시글의 상세 정보를 조회합니다.")
     @GetMapping("/{postId}")
     ResponseEntity<ApiResponse<GetPostDetailResponse>> getDetailPost(
             @RequestParam Long userId,
-            @Valid @PathVariable Long postId
+            @PathVariable Long postId
     );
 
     @Operation(summary = "게시글 생성", description = "새로운 게시글을 생성합니다.")
