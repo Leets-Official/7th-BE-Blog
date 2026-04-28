87a8a8d2bb1565dad964bfdbfc2a36c7a0bfb924
jihoonkim501 <jihungim500@gmail.com>
2026-04-28 23:01:18 +0900

    [Delete] #130 불필요한 import 삭제

--- src/main/java/com/example/leets_7th/domain/post/validator/PostValidator.java
+++ src/main/java/com/example/leets_7th/domain/post/validator/PostValidator.java
@@ -4,8 +4,6 @@ import com.example.leets_7th.common.exception.GeneralException;
 import com.example.leets_7th.common.status.ErrorStatus;
 import com.example.leets_7th.domain.post.entity.Post;
 import com.example.leets_7th.domain.post.repository.PostRepository;
-import com.example.leets_7th.domain.user.entity.User;
-import com.example.leets_7th.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
