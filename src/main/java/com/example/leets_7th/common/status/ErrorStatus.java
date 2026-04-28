538d38df29062de81c42c4084436408d2c6a4889
jihoonkim501 <jihungim500@gmail.com>
2026-04-28 22:54:23 +0900

    [Feat] #130 성공/실패 status 추가

--- src/main/java/com/example/leets_7th/common/status/ErrorStatus.java
+++ src/main/java/com/example/leets_7th/common/status/ErrorStatus.java
@@ -27,8 +27,23 @@ public enum ErrorStatus implements BaseStatus {
 
 
     // Post
-    POST_NOT_FOUND(HttpStatus.NOT_FOUND,"POST_404","게시글을 찾을 수 없습니다."),
-    POST_ALREADY_DELETED(HttpStatus.BAD_REQUEST,"POST_400","이미 삭제된 게시글입니다.");
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND,"POST_404","게시글이 존재하지 않습니다."),
+    POST_ALREADY_DELETED(HttpStatus.BAD_REQUEST,"POST_400","이미 삭제된 게시글입니다."),
+    FORBIDDEN_COMMENT_UPDATE(HttpStatus.FORBIDDEN,"POST_403","게시글을 수정할 권한이 없습니다."),
+    ALREADY_LIKED_POST(HttpStatus.CONFLICT, "POST_409", "이미 좋아요를 누른 게시글입니다."),
+    POST_LIKE_NOT_FOUND(HttpStatus.NOT_FOUND,"POST_404","게시글에 좋아요 취소할 좋아요가 없습니다."),
+    ALREADY_REPORTED_POST(HttpStatus.CONFLICT, "POST_409", "이미 신고한 게시글입니다."),
+
+
+    // Comment
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND,"COMM_4041","게시글 또는 댓글이 존재하지 않습니다."),
+    ALREADY_LIKED_COMMENT(HttpStatus.BAD_REQUEST,"COMM_4001","이미 좋아요를 누른 댓글입니다."),
+    FORBIDDEN_COMMENT_DELETE(HttpStatus.FORBIDDEN,"COMM_4031","댓글을 삭제할 권한이 없습니다."),
+    COMMENT_LIKE_COUNT_INVALID(HttpStatus.BAD_REQUEST,"COMM_4002","좋아요 수는 0보다 작을 수 없습니다."),
+    COMMENT_LIKE_NOT_FOUND(HttpStatus.NOT_FOUND,"COMM_4042","좋아요를 누르지 않은 댓글입니다."),
+    ALREADY_REPORTED_COMMENT(HttpStatus.BAD_REQUEST,"COMM_4003","이미 신고한 댓글입니다."),
+    DELETED_COMMENT(HttpStatus.NO_CONTENT,"COMM_2041","삭제된 댓글입니다."),
+    HIDDEN_COMMENT(HttpStatus.NO_CONTENT,"COMM_2042","신고로 인해 숨겨진 댓글입니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
--- src/main/java/com/example/leets_7th/common/status/SuccessStatus.java
+++ src/main/java/com/example/leets_7th/common/status/SuccessStatus.java
@@ -13,12 +13,24 @@ public enum SuccessStatus implements BaseStatus {
     HEALTH_CHECK_SUCCESS_STATUS(HttpStatus.OK, "TEST_200", "OK"),
     STRING_REPEAT_SUCCESS(HttpStatus.CREATED, "TEST_201", "문자열을 성공적으로 출력했습니다."),
 
+
+    // Post
     GET_ALL_POST_SUCCESS(HttpStatus.OK, "POST_2001", "게시글 목록 조회에 성공했습니다."),
     GET_POST_DETAIL_SUCCESS(HttpStatus.OK, "POST_2002", "게시글 상세 조회에 성공했습니다."),
     CREATE_POST_SUCCESS(HttpStatus.CREATED, "POST_2011", "게시글 생성에 성공했습니다."),
     UPDATE_POST_SUCCESS(HttpStatus.CREATED, "POST_2003", "게시글 수정에 성공했습니다."),
-    DELETE_POST_SUCCESS(HttpStatus.OK, "POST_2004", "게시글 삭제에 성공했습니다.");
+    DELETE_POST_SUCCESS(HttpStatus.OK, "POST_2004", "게시글 삭제에 성공했습니다."),
+    LIKE_POST_SUCCESS(HttpStatus.OK,"POST_2005","게시글 좋아요에 성공했습니다."),
+    UNLIKE_POST_CANCEL_SUCCESS(HttpStatus.OK,"POST_2006","게시글 좋아요 취소에 성공했습니다."),
+    REPORT_POST_SUCCESS(HttpStatus.OK,"POST_2007","게시글 신고에 성공했습니다."),
 
+    // Comment
+    CREATE_COMMENT_SUCCESS(HttpStatus.CREATED,"COMM_2011","댓글 생성에 성공했습니다."),
+    UPDATE_COMMENT_SUCCESS(HttpStatus.CREATED, "COMM_2003", "댓글 수정에 성공했습니다."),
+    DELETE_COMMENT_SUCCESS(HttpStatus.OK, "COMM_2004", "댓글 삭제에 성공했습니다."),
+    REPORT_COMMENT_SUCCESS(HttpStatus.OK,"COMM_REPORT_2001","댓글 신고에 성공했습니다."),
+    LIKE_COMMENT_SUCCESS(HttpStatus.OK,"LIKE_2001","댓글 좋아요에 성공했습니다."),
+    UNLIKE_COMMENT_CANCEL_SUCCESS(HttpStatus.OK,"UNLIKE_2001","댓글 좋아요 취소에 성공했습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
