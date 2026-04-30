b9e3dabbe277323940d5390db31ee1ecaac30eac
Seonghyeon Lim <lsh0131pianist@gmail.com>
2026-04-30 18:14:48 +0900

    Revert "[4주차] 정대교/[feat] 추가 API 구현"

--- src/main/java/com/example/springbootassignment/domain/report/entity/ReportRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.springbootassignment.domain.report.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReportRepository extends JpaRepository<Report, Long> {
-}
