502c883cfccd249fb9147edd87c5dc4c7f9b9ec3
kallin <kedia1004@gmail.com>
2026-05-12 20:07:19 +0900

    feat: 6주차 토큰기반 로그인/회원가입로직구현

---
+++ kimdain/src/main/java/com/leets/blog/auth/dto/TokenResponse.java
@@ -0,0 +1,10 @@
+package com.leets.blog.auth.dto;
+
+public record TokenResponse(
+        String accessToken,
+        String refreshToken
+) {
+    public static TokenResponse of(String accessToken, String refreshToken) {
+        return new TokenResponse(accessToken, refreshToken);
+    }
+}
--- kimdain/src/main/java/com/leets/blog/repository/UserRepository.java
+++ kimdain/src/main/java/com/leets/blog/repository/UserRepository.java
@@ -2,8 +2,10 @@ package com.leets.blog.repository;
 
 import com.leets.blog.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
 }
