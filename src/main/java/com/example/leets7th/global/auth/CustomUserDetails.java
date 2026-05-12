b8dd17bd011ec0ac85e26269b0268295842d2316
kangyeson <yesonkang@gmail.com>
2026-05-12 17:00:46 +0900

    [Feat] UserDetails로 getUserId() 구현 및 이메일 유저 로드 기능 구현

---
+++ src/main/java/com/example/leets7th/global/auth/CustomUserDetails.java
@@ -0,0 +1,39 @@
+package com.example.leets7th.global.auth;
+
+import com.example.leets7th.domain.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public Long getUserId() {
+        return user.getId();
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+}
