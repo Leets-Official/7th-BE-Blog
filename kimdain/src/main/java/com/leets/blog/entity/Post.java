78d880d1e68759470aa2128ddbced4bd7c65ce12
kallin1 <kedia1004@gmail.com>
2026-05-12 20:04:03 +0900

    Revert "[6주차] 김다인/[feat] Spring Security + JWT 인증/인가"

--- kimdain/src/main/java/com/leets/blog/entity/Post.java
+++ kimdain/src/main/java/com/leets/blog/entity/Post.java
@@ -1,9 +1,8 @@
 package com.leets.blog.entity;
 
-import com.leets.blog.entity.enums.ContentStatus;
 import jakarta.persistence.*;
 import lombok.*;
-import lombok.experimental.SuperBuilder;
+
 import java.util.List;
 
 @Entity
@@ -11,8 +10,8 @@ import java.util.List;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-@SuperBuilder // 상속 구조에는 SuperBuilder가 필수입니다
-public class Post extends BaseEntity implements Reportable {
+@Builder
+public class Post extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,7 +24,7 @@ public class Post extends BaseEntity implements Reportable {
     @Column(name = "title", nullable = false, length = 100)
     private String title;
 
-    @Column(columnDefinition = "TEXT", nullable = false) // TEXT 타입이 더 안전합니다
+    @Column(name = "content", nullable = false, length = 255)
     private String content;
 
     @Column(name = "image", length = 255)
@@ -36,41 +35,4 @@ public class Post extends BaseEntity implements Reportable {
 
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
     private List<PostLike> postLikes;
-
-    // --- 신고 시스템 (Reportable) ---
-    @Enumerated(EnumType.STRING)
-    @Column(name = "status")
-    @Builder.Default
-    private ContentStatus status = ContentStatus.ACTIVE;
-
-    @Column(name = "total_weight")
-    @Builder.Default
-    private int totalWeight = 0;
-
-    // --- 비즈니스 로직 ---
-    public void update(String title, String content) {
-        this.title = title;
-        this.content = content;
-        this.updatedAt = java.time.LocalDateTime.now();
-    }
-
-    public void delete() {
-        this.deletedAt = java.time.LocalDateTime.now();
-    }
-
-    // --- Reportable 인터페이스 구현 ---
-    @Override
-    public void addWeight(int weight) {
-        this.totalWeight += weight;
-    }
-
-    @Override
-    public int getTotalWeight() {
-        return this.totalWeight;
-    }
-
-    @Override
-    public void updateStatus(ContentStatus status) {
-        this.status = status;
-    }
 }
--- kimdain/src/main/java/com/leets/blog/service/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.leets.blog.repository;
-
-import com.leets.blog.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
