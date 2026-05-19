4d30ef053e2defe063a1541246170dea02d96ecb
daekyochung <chung5817@gachon.ac.kr>
2026-05-19 23:47:16 +0900

    7주차 과제 제출

--- src/main/java/com/example/springbootassignment/controller/AuthController.java
+++ src/main/java/com/example/springbootassignment/controller/AuthController.java
@@ -6,7 +6,11 @@ import com.example.springbootassignment.dto.TokenResponse;
 import com.example.springbootassignment.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
@@ -25,5 +29,23 @@ public class AuthController {
     public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest request) {
         TokenResponse tokenResponse = authService.login(request);
         return ResponseEntity.ok(tokenResponse);
+    }
+
+    @GetMapping("/loginSuccess")
+    public String loginSuccess(Authentication authentication) {
+
+        OAuth2User oAuth2User =
+                (OAuth2User) authentication.getPrincipal();
+
+        Map<String, Object> attributes =
+                oAuth2User.getAttributes();
+
+        Map<String, Object> kakaoAccount =
+                (Map<String, Object>) attributes.get("kakao_account");
+
+        String email =
+                (String) kakaoAccount.get("email");
+
+        return "카카오 로그인 성공! 이메일 : " + email;
     }
 }
