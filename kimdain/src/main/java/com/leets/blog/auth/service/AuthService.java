f1dbf68acb6500715c98fa7f575239d236bea553
kallin <kedia1004@gmail.com>
2026-05-27 00:08:05 +0900

    chore: CI/CD 재트리거

--- kimdain/src/main/java/com/leets/blog/auth/service/AuthService.java
+++ kimdain/src/main/java/com/leets/blog/auth/service/AuthService.java
@@ -56,6 +56,7 @@ public class AuthService {
                         .password(passwordEncoder.encode(kakaoId))
                         .build()));
 
+
         return TokenResponse.of(
                 jwtProvider.createAccessToken(user.getEmail()),
                 jwtProvider.createRefreshToken(user.getEmail())
